7a855df93592916e4f6b05db4b2fd8d2558798f1
Lars Vermaas <431470@student.saxion.nl>
2019-03-13 17:37:06 +0100

    fist program

---
+++ program.pl
@@ -0,0 +1,14 @@
+% man(Person)
+% it is true if Person is a man
+
+man(justuuuuuuuuuus).
+man(lars).
+man(teun).
+man(sebastian).
+
+% woman(Person).
+% it is true if Person is a woman
+
+woman(wendele).
+woman(frederique).
+woman(lisanne).
