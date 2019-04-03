e70818a362df6e27648131f3d8a98cc076ea0e8e
lars09 <48525224+lars09@users.noreply.github.com>
2019-04-03 17:34:12 +0200

    Add files via upload

---
+++ peano.pl
@@ -0,0 +1,18 @@
+
+
+substr(X, 0, X).
+substr(X, n(Y), Z):- substr(X, Y, n(Z)).
+
+d2p(0,0).
+d2p(D, n(P)):- D > 0, D2 is - 1, d2p(D2,P).
+
+
+% p2d(+P, -D)
+% it is true if D unify with a 
+% decimal number equivalent to the
+% Peano representation of P
+
+p2d(0, 0).
+p2d(n(X), D2):- p2d(X, D), D2 is D + 1.
+
+
