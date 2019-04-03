5cca079d16c650b44454ab50a4522964ce3527da
lars09 <48525224+lars09@users.noreply.github.com>
2019-04-03 18:02:13 +0200

    Add files via upload

--- peano.pl
+++ peano.pl
@@ -15,4 +15,13 @@ d2p(D, n(P)):- D > 0, D2 is - 1, d2p(D2,P).
 p2d(0, 0).
 p2d(n(X), D2):- p2d(X, D), D2 is D + 1.
 
+% times(?X, ?Y, ?Z)
+% it is true if Z unify with X * Y in Peano Arithmetic
+% it is true if Z unify with a Peano number
+% equivalent to add X, Y times
+
+times(_, 0, 0).
+
+times(X, n(Y), R):- times (X, Y, Z), sum(X, Z, R).
+
 
