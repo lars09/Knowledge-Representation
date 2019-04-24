5364ae5cd880b791e4119d9453dc0879bf89d07a
lars09 <48525224+lars09@users.noreply.github.com>
2019-04-24 18:01:45 +0200

    Add files via upload

---
+++ bubbleSort.pl
@@ -0,0 +1,25 @@
+
+% bubbleSort)+List, -Result)
+% it is true if Result unify with a list
+% with same elements than List but ordered
+% from lowest to highest.
+
+bubbleSort(List, R):-
+  append(L1, [E1, E2|L2], List),
+  E1 > E2,
+  append(L1, [E2, E1|L2], List2),
+  bubbleSort(List2, R).
+  
+  
+bubbleSort(List, List):- ordered(List).
+
+% ordered(+List)
+
+% it is true if List elements are ordered from
+% lowest to highest
+
+ordered([]).
+
+ordered([E1, E2| Tail]):- E1 =< E2,
+  ordered([E2|Tail]).
+  
